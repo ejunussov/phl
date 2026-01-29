96542e3337282f9e820e1f687d6fe0a7ac16ae48
Eduard Junussov <ed.junussov@parkland.ca>
2026-01-28 16:51:44 -0800

    Update EIA mogas S&D reports and assets

--- publish.bat
+++ publish.bat
@@ -8,7 +8,7 @@ set SRC_DIR=C:\R\padd_reports
 set DEST_DIR=C:\R\phl
 
 REM =========================
-REM Ensure destination exists
+REM Sanity check
 REM =========================
 if not exist "%DEST_DIR%" (
     echo Destination repo does not exist
@@ -19,27 +19,28 @@ REM =========================
 REM Copy files
 REM =========================
 copy /Y "%SRC_DIR%\eia_padd_1_mogas_snd.html" "%DEST_DIR%\"
+copy /Y "%SRC_DIR%\eia_padd_3_mogas_snd.html" "%DEST_DIR%\"
 copy /Y "%SRC_DIR%\eia_report.html" "%DEST_DIR%\"
 
 REM Copy entire assets directory
 xcopy "%SRC_DIR%\eia_report_files\*" "%DEST_DIR%\eia_report_files\" /E /I /Y >nul
 
 REM =========================
-REM Git: one combined commit
+REM Git: single combined commit
 REM =========================
 cd /d "%DEST_DIR%" || exit /b 1
 
-REM Stage *all* changes (new, modified, deleted)
+REM Stage all changes (new / modified / deleted)
 git add -A
 
-REM Commit only if there are changes
+REM Skip commit if nothing changed
 git diff --cached --quiet
 if %errorlevel%==0 (
     echo No changes to commit
     exit /b 0
 )
 
-git commit -m "Update EIA reports and assets"
+git commit -m "Update EIA mogas S&D reports and assets"
 git push
 
 endlocal
